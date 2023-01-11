bace150a379df7d6443b9bd354022e6c20430fb0
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2023-01-11 23:29:43 +0545

    Create __main__.py

---
+++ plugins/custom/rkdb/__main__.py
@@ -0,0 +1,48 @@
+import aiohttp
+import asyncio
+import openai
+from userge import userge, Message
+
+async def fetch(session, url):
+    try:
+        async with session.get(url) as response:
+            return await response.json()
+    except aiohttp.ClientError as err:
+        print(f'An error occurred while trying to fetch data from {url}: {err}')
+
+def description(movieDet, magnetLink):
+    descriptions = f"<b>Title</b><a href='{movieDet['image']}'>🎬</a>: <code>{movieDet['title']}</code>"
+    descriptions += f"""
+<b>Genres: </b><code>{' '.join(movieDet['genre']) if len(movieDet['genre']) > 0 else ''}</code>
+<b>Rating⭐: </b><code>{movieDet['rating']['star']}</code>
+<b>Magnet Link🔗: </b>{magnetLink}
+<b>IMDB URL Link🔗: </b>{movieDet['imdb']}
+<b>Story Line : </b><em>{movieDet['plot']}</em>"""
+    print(descriptions)
+
+@userge.on_cmd("rkdb", about={
+    'header': "Scrap Movies & Tv Shows from IMDB ",
+    'description': "Get info about a Movie on IMDB with magnet url.\n",
+    'usage': "!rkdb moviename"})
+async def main(message: Message):
+    movie_name = message.input_str
+    async with aiohttp.ClientSession() as session:
+        try:
+            search_url = f'https://imdb-api.tprojects.workers.dev/search?query={movie_name}'
+            movie_id = (await fetch(session, search_url))['results'][0]['id']
+            magnetURL = ""
+            title_url = f'https://imdb-api.tprojects.workers.dev/title/{movie_id}'
+            final_result = await fetch(session, title_url)
+            if final_result['contentType'] == "Movie":
+                movieTorURL = f'https://yts.mx/api/v2/list_movies.json?query_term={movie_id}'
+                movieMagnet = await fetch(session, movieTorURL)
+                magnetURL = movieMagnet['data']['movies'][0]['torrents'][0]['url']
+            description(final_result, magnetURL)
+        except aiohttp.ClientError as err:
+            print(f'An error occurred while trying to fetch movie information: {err}')
+        except IndexError as err:
+            print(f'An error occurred while trying to access the results: {err}')
+        except KeyError as err:
+            print(f'<b>Brush enter valid movie name<b>')
+        except Exception as err:
+            print(f'An unknown error occurred: {err}')
