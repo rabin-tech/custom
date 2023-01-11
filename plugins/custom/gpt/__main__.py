34e48f8a459c6ae25f16152cb5e14846eabc45a1
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2023-01-11 15:02:03 +0545

    Create __main__.py

---
+++ plugins/custom/gpt/__main__.py
@@ -0,0 +1,19 @@
+import openai
+from userge import userge, Message
+
+openai.api_key = "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"
+
+@userge.on_cmd("gpt", about={
+    'header': "ChatGPT bot by Yagami",
+    'usage': "!gpt question"})
+async def get_answer(message: Message):
+          question = message.input_str
+          response = openai.Completion.create(
+          engine="text-davinci-002",
+          prompt=(f"{question}\n"),
+          max_tokens=4000,
+          n=1,
+          stop=None,
+          temperature=0.7,
+      )
+          await message.edit(response["choices"][0]["text"])
