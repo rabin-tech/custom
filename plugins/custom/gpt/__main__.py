e193834dbcc8f87e0a2679310dca7403037cb56e
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2023-01-11 17:34:43 +0545

    Update __main__.py

--- plugins/custom/gpt/__main__.py
+++ plugins/custom/gpt/__main__.py
@@ -10,11 +10,11 @@ openai.api_key = gpt.GPT_KEY
 async def get_answer(message: Message):
           question = message.input_str
           response = openai.Completion.create(
-          engine="text-davinci-002",
+          engine="text-davinci-003",
           prompt=(f"{question}\n"),
           max_tokens=4000,
           n=1,
           stop=None,
           temperature=0.7,
       )
-          await message.edit(response["choices"][0]["text"])
+          await message.edit(f"GPT:\n<i>response["choices"][0]["text"]</i>")
