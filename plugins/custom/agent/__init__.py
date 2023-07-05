628c490899b1674d34b53b45456d07b0ade680c8
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2023-07-05 18:19:30 +0545

    Update __init__.py

--- plugins/custom/agent/__init__.py
+++ plugins/custom/agent/__init__.py
@@ -1,1 +1,19 @@
+""" this thing will be used as plugin doc string """
 
+# here you can do initializing things or keep shared data which will be used by other plugins
+
+""" get gpt messages """
+
+import os
+
+# this is a constant (not going to change)
+API_KEY = os.getenv("API_KEY")
+GPT_KEY = os.environ.get("GPT_KEY")
+SERPAPI_API_KEY = os.environ.get("SERPAPI_API_KEY ")
+# these values can be changed in runtime
+class Dynamic:
+    TIMEOUT = 60
+
+
+def shared_method() -> None:
+    pass
