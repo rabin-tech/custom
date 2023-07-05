aede08ac0ee4572dfe4b3906e25a580816ac3857
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2023-07-05 19:18:24 +0545

    Update __main__.py

--- plugins/custom/agent/__main__.py
+++ plugins/custom/agent/__main__.py
@@ -7,7 +7,7 @@ from langchain.llms import OpenAI
 @userge.on_cmd("agent", about={
     'header': "Agent bot by Yagami",
     'usage': "!agent question"})
-async def get_answer(message: Message):
+def get_answer(message: Message):
           question = message.input_str
           llm = OpenAI(openai_api_key= gpt.GPT_KEY, temperature=0.6)
           os.environ["SERPAPI_API_KEY"] = gpt.SERPAPI_API_KEY
@@ -17,5 +17,5 @@ async def get_answer(message: Message):
           llm,
           agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION
           )
-          await response = agent.run(question)
-          await message.edit(f"GPT:\n<i>{response}<i>")
+          response = agent.run(question)
+          message.edit(f"GPT:\n<i>{response}<i>")
