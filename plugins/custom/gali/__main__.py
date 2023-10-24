089cc78e05f83f58d1b5475b4717c3b7fe64708c
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2023-10-24 11:25:39 +0545

    Update __main__.py

--- plugins/custom/gali/__main__.py
+++ plugins/custom/gali/__main__.py
@@ -1,1 +1,83 @@
+import random
+from userge import userge, Message
 
+messages =[
+    'Chup mug',
+    'disturb vo mug',
+    'https://media.tenor.com/images/ee616b63bab2fa326e867f452235894a/tenor.gif',
+    'guu kha mug 💩',
+    'https://i.imgur.com/M3OhyYn.jpg',
+    'lyang lyang na han na lado',
+    '<:muji:864716123500183611>',
+    'https://media.tenor.com/images/8e834a7c1807ac17265c11071b1c5748/tenor.gif',
+    'jurukka uthera jhyamma mukh padkau!',
+    'Sutna dyao, chuppa lara!',
+    'marna nasakya randi ko xoro muji',
+    'https://media.tenor.com/images/4da49d51af9f989e520080b7557e050c/tenor.gif',
+    'lado chus machikne',
+    'muji sarki',
+    'lado kha',
+    'tero bauko kalo condo',
+    'tero bau',
+    'teri aama ko chamro puti',
+    'machikne randi kina bolais',
+    'bhalu ko xori randi',
+    'uttano khana paryo radi talai?',
+    'bhutro khojeko ho muji?',
+    'thukka machikne randiko xoroharu',
+    'तेरो बाउको झिलझिले कट्टु',
+    'Chimma abu',
+    'Mampakha Naku na',
+    'Randi ko chak beshya chikauti',
+    'tero bajeko naito',
+    'thukka, sala masti ko natija',
+    'Hariyo bhann nepal ko dhann\ntero aama randi ko ban',
+    'English ma Ear\nNepali ma kaan\nChuplag khate muji randikoban',
+    'yo muji pakhe feri aayo',
+    'tero baje ko turi',
+    'vaisi ko lado kha machikne',
+    'खसि बलात्कारी माचिक्ने',
+    'randiko xoro bahun machikne',
+    'Mathyasni randi ko baan',
+    'Naak katdim ta mug newarko?',
+    'Muji rautey',
+    'Badi nabol muji',
+    'Machikney randi ko ban chup lag',
+    'Maa sala khatey ko ban',
+    'randi bhalu',
+    'halla nagar chikney!',
+    'Geda khana karaira?',
+    'lado chus muji randi',
+    'radi ko xoro puti mukhe',
+    'hijada radi',
+    'terima dharti ko bojh machikne',
+    'condom futera janmeko randi kina badi bolira',
+    'moran chyase chikne furti lagauxas mug',
+    'mero fushi kha machikne',
+    'gula kha randi ko ban',
+    'chuplag radi ko puti',
+    'turi tauke sala',
+    'khatey ko ban',
+    'Ranmati',
+    'ahile rando ko kando phutaidinxu ani tha hunxa',
+    'aija muji single single',
+    'hasais machikney',
+    'Turi tandim tero?',
+    'kasto muji gula jasto raixa chikne',
+    'https://media.tenor.com/images/fa8ceb000dad3a6b5d34fdc002530715/tenor.gif',
+    'class ma jaa chikne. tero mom lai vandim?',
+    'k vo mug?',
+    'jaa mug tiktok bana. k discord chalauxas ta',
+    'khate bahun',
+    'https://media.tenor.com/images/473f1d3b5df4ce28d7ce53ffd8bfd9bd/tenor.gif',
+    'lati ko poi'
+]
+
+@userge.on_text("oa")
+async def send_random_message(message):
+    random_message = random.choice(messages)
+    await message.reply(random_message)
+
+
+
+   
